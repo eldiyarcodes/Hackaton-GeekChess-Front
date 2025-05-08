e23090aea46126479945b8095e70e7b1df989d18
Eldyar <thedolbekov2@gmail.com>
2025-05-08 04:46:36 +0300

    refactor: add new route

--- src/app/config/app.tsx
+++ src/app/config/app.tsx
@@ -2,6 +2,7 @@ import type { FC } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { AuthPage } from '../../pages/auth/auth-page'
 import { GameRoom } from '../../pages/game-room/game-room'
+import { IntroPage } from '../../pages/intro/intro-page'
 import { AppRoutes } from '../../shared/utils/consts/consts'
 
 export const App: FC = () => {
@@ -9,6 +10,7 @@ export const App: FC = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path={AppRoutes.HOME} element={<AuthPage />} />
+				<Route path={AppRoutes.INTRO} element={<IntroPage />} />
 				<Route path={AppRoutes.GAME_ROOM} element={<GameRoom />} />
 				<Route path={AppRoutes.NOT_FOUND} element={<div>Page Not Found</div>} />
 			</Routes>
