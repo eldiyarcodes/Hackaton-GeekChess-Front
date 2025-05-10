a588f826e416b806eb540da91052546c5dcb0037
Eldyar <thedolbekov2@gmail.com>
2025-05-10 09:53:33 +0300

    commit

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,56 +1,56 @@
-import clsx from 'clsx'
-import { motion } from 'framer-motion'
-import type { FC, ReactNode } from 'react'
-import { createPortal } from 'react-dom'
-import { useDisableScroll } from '../../hooks/use-disable-scroll'
-import classes from './modal.module.scss'
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import type { FC, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+import { useDisableScroll } from '../../hooks/use-disable-scroll';
+import classes from './modal.module.scss';
 
 interface IModalProps {
-	isOpen: boolean
-	children?: ReactNode
-	title?: string
-	className?: string
-	onClick?: () => void
+  isOpen: boolean;
+  children?: ReactNode;
+  title?: string;
+  className?: string;
+  onClick?: () => void;
 }
 
 export const Modal: FC<IModalProps> = ({
-	isOpen,
-	children,
-	title,
-	className,
+  isOpen,
+  children,
+  title,
+  className,
 }) => {
-	useDisableScroll(isOpen)
+  useDisableScroll(isOpen);
 
-	if (!isOpen) {
-		return null
-	}
+  if (!isOpen) {
+    return null;
+  }
 
-	return createPortal(
-		<motion.div
-			layout
-			className={clsx(classes.modalBackdrop, classes.visible)}
-			initial={{ opacity: 0 }}
-			animate={{ opacity: isOpen ? 1 : 0 }}
-			exit={{ opacity: 0 }}
-			transition={{ duration: 0.2 }}
-			style={{ pointerEvents: 'auto' }}
-		>
-			<motion.div
-				className={clsx(classes.modalContent, className)}
-				initial={{ scale: 0.8 }}
-				animate={{ scale: isOpen ? 1 : 0.8 }}
-				exit={{ scale: 0.8 }}
-				transition={{ duration: 0.2 }}
-				onClick={e => e.stopPropagation()}
-			>
-				{title && (
-					<div className={classes.title}>
-						<p>{title}</p>
-					</div>
-				)}
-				{children}
-			</motion.div>
-		</motion.div>,
-		document.body
-	)
-}
+  return createPortal(
+    <motion.div
+      layout
+      className={clsx(classes.modalBackdrop, classes.visible)}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isOpen ? 1 : 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      style={{ pointerEvents: 'auto' }}
+    >
+      <motion.div
+        className={clsx(classes.modalContent, className)}
+        initial={{ scale: 0.8 }}
+        animate={{ scale: isOpen ? 1 : 0.8 }}
+        exit={{ scale: 0.8 }}
+        transition={{ duration: 0.2 }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {title && (
+          <div className={classes.title}>
+            <p>{title}</p>
+          </div>
+        )}
+        {children}
+      </motion.div>
+    </motion.div>,
+    document.body
+  );
+};
--- src/shared/utils/consts/consts.ts
+++ src/shared/utils/consts/consts.ts
@@ -1,52 +1,53 @@
 export const AppRoutes = {
-	HOME: '/',
-	GAME_ROOM: '/game',
-	INTRO: '/intro',
-	NOT_FOUND: '*',
-} as const
+  HOME: '/',
+  GAME_ROOM: '/game',
+  INTRO: '/intro',
+  NOT_FOUND: '*',
+} as const;
 
 export const baseURL = import.meta.env.VITE_API_URL;
 
 export const CoinNaminals = {
-	COIN150: 150,
-	COIN200: 200,
-	COIN250: 250,
-	COIN300: 300,
-	COIN350: 350,
-} as const
+  COIN150: 150,
+  COIN200: 200,
+  COIN250: 250,
+  COIN300: 300,
+  COIN350: 350,
+} as const;
 
 export const Colors = {
-	WHITE: 'white',
-	BLACK: 'black',
-} as const
+  WHITE: 'white',
+  BLACK: 'black',
+} as const;
 
 export const FigureNames = {
-	FIGURE: 'Фигура',
-	KING: 'Король',
-	QUEEN: 'Ферзь',
-	KNIGHT: 'Конь',
-	PAWN: 'Пешка',
-	ROOK: 'Ладья',
-	BISHOP: 'Слон',
-} as const
+  FIGURE: 'Фигура',
+  KING: 'Король',
+  QUEEN: 'Ферзь',
+  KNIGHT: 'Конь',
+  PAWN: 'Пешка',
+  ROOK: 'Ладья',
+  BISHOP: 'Слон',
+} as const;
 
 export const Tokens = {
-	ACCESS: 'token_auth',
-}
+  ACCESS: 'token_auth',
+};
 export const CurrentPlayer = 'CURRENT_PLAYER';
 
+export const GAME_TIMER = 60;
 
-export const GAME_TIMER = 10
-
-export type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes]
-export type CoinNaminals = (typeof CoinNaminals)[keyof typeof CoinNaminals]
-export type Colors = (typeof Colors)[keyof typeof Colors]
-export type FigureNames = (typeof FigureNames)[keyof typeof FigureNames]
-
+export type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
+export type CoinNaminals = (typeof CoinNaminals)[keyof typeof CoinNaminals];
+export type Colors = (typeof Colors)[keyof typeof Colors];
+export type FigureNames = (typeof FigureNames)[keyof typeof FigureNames];
 
 export const rules = [
-	{id: 1, text: 'Игрок управляет только конем'},
-	{id: 2, text: 'На поле появляются GeekCoin'},
-	{id: 3, text: 'Задача: Собрать как можно больше GeekCoin за ограниченное число времени'},
-	{id: 4, text: 'Только допустимые ходы коня (буквой “Г”)'}
-]
+  { id: 1, text: 'Игрок управляет только конем' },
+  { id: 2, text: 'На поле появляются GeekCoin' },
+  {
+    id: 3,
+    text: 'Задача: Собрать как можно больше GeekCoin за ограниченное число времени',
+  },
+  { id: 4, text: 'Только допустимые ходы коня (буквой “Г”)' },
+];
